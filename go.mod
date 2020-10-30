cd52c4e2a17ae794a6cc5de4bd4d1b4e69b0d519
Richard Wilkes <wilkes@me.com>
2020-10-30 08:46:29 -0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/richardwilkes/md
+
+go 1.15
+
+require (
+	github.com/richardwilkes/toolbox v1.30.0
+	github.com/yuin/goldmark v1.2.1
+)
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/richardwilkes/toolbox/atexit"
+	"github.com/richardwilkes/toolbox/cmdline"
+	"github.com/richardwilkes/toolbox/log/jot"
+	"github.com/richardwilkes/toolbox/xio/fs"
+	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/extension"
+	"github.com/yuin/goldmark/parser"
+	"github.com/yuin/goldmark/renderer/html"
+)
+
+func main() {
+	cmdline.AppName = "MarkDown"
+	cmdline.AppCmdName = "md"
+	cmdline.License = "Mozilla Public License, version 2.0"
+	cmdline.CopyrightYears = "2020"
+	cmdline.CopyrightHolder = "Richard A. Wilkes"
+	cl := cmdline.New(true)
+	md := goldmark.New(
+		goldmark.WithParserOptions(
+			parser.WithAutoHeadingID(),
+			parser.WithAttribute(),
+		),
+		goldmark.WithRendererOptions(html.WithUnsafe()),
+		goldmark.WithExtensions(
+			extension.GFM,
+			extension.NewTypographer(),
+		))
+	for _, p := range cl.Parse(os.Args[1:]) {
+		if filepath.Ext(p) != ".md" {
+			jot.Warn("skipping non-markdown file: " + p)
+			continue
+		}
+		data, err := ioutil.ReadFile(p)
+		jot.FatalIfErr(err)
+		var f *os.File
+		f, err = os.Create(fs.TrimExtension(p) + ".html")
+		jot.FatalIfErr(err)
+		jot.FatalIfErr(md.Convert(data, f))
+		jot.FatalIfErr(f.Close())
+	}
+	atexit.Exit(0)
+}
